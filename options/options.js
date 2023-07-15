f7ad7ddc6b834afd3d16b5b7e7f52baed4d068e8
Crowdbotics <team@crowdbotics.com>
2023-07-15 18:21:57 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "divine-credit-42205",
+  url: "https://divine-credit-42205.botics.co",
+  api: "https://divine-credit-42205.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
